54900f0f8f9577c4aa9b97ee2f1b0d540e0ef01c
washington <washingtonmoreiraevangelista@gmail.com>
2026-02-16 14:19:41 -0300

    feat: add renderContactMessages function to display contact messages in admin panel

--- src/modules/admin/load-admin.js
+++ src/modules/admin/load-admin.js
@@ -3,7 +3,7 @@ import { handleLogout } from "../auth/handle-logout"
 import { schedulesDayAdmin } from "../admin/schedules-admin"
 import { initAdminEvents } from "../admin/init-admin"
 import { listServices } from './list-services'
-import { renderContactMessages } from './contatct-messages'
+import { renderContactMessages } from './contact-messages'
 
 
 
